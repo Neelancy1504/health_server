797713f4f606eb3de0d6f525bc7bbf9f01d84008
Sahil Kumar <sahilkumar.sk148@gmail.com>
2025-04-26 15:18:28 +0530

    Fix Role Route

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -1,18 +1,17 @@
 const verifyRole = (allowedRoles) => {
-    return (req, res, next) => {
-      if (!req.user) {
-        return res.status(401).json({ message: "User not authenticated" });
-      }
-  
-      const userRole = req.user.role; // we'll pass role in token later
-  
-      if (!allowedRoles.includes(userRole)) {
-        return res.status(403).json({ message: "Access denied: insufficient permissions" });
-      }
-  
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    if (allowedRoles.includes(req.user.role)) {
       next();
-    };
+    } else {
+      return res
+        .status(403)
+        .json({ message: "Access denied: insufficient permissions" });
+    }
   };
-  
-  module.exports = verifyRole;
-  
+};
+
+module.exports = verifyRole;
